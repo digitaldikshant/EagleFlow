bc70dc471b2fd0fe0e06497e0c4f4857dab85725
My PC <My PC@LAPTOP-112OIFP5>
2026-02-15 15:11:47 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EagleFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
